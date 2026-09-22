140ccae65658b4ee2cbd5cc320af0c6de8ad2ffe
dolphindb049 <dolphindb049@users.noreply.github.com>
2026-09-22 17:15:37 +0800

    Clean legacy source header formatting

--- Tushare数据导入/basic.py
+++ Tushare数据导入/basic.py
@@ -3,7 +3,6 @@ Author: DolphinDB
 Date: 2025-10-21 16:22:33
 LastEditors: DolphinDB
 LastEditTime: 2025-10-22 11:58:30
-FilePath: \PythonModules\TushareToDDB\basic.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 #!/usr/bin/env python
